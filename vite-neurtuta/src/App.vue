96df1e68030de11516ffb461973e2fc6d3578d35
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-06-24 12:19:51 +0100

    first commit

---
+++ vite-neurtuta/src/App.vue
@@ -0,0 +1,102 @@
+<script>
+import axios from "axios";
+import { store } from './store';
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    newCaloriesCalculation() {
+      let caloriesBMI = 0;
+
+      if (store.myData.gender === 'male') {
+        caloriesBMI = (10 * store.myData.weight + 6.25 * store.myData.height - 5 * store.myData.age + 5) * 1.1;
+
+      } else if (store.myData.gender === 'female') {
+        caloriesBMI = (10 * store.myData.weight + 6.25 * store.myData.height - 5 * store.myData.age - 161) * 1.1;
+      };
+
+      caloriesBMI = store.myData.caloriesBMI;
+
+      const caloriesWithWO = caloriesBMI * parseFloat(store.myData.numberWorkouts);
+      console.log(caloriesWithWO);
+      console.log(typeof caloriesWithWO);
+
+
+      // console.log(store.myData.calories);
+      // console.log(caloriesWithWO);
+
+    },
+    debugbutton() {
+
+    }
+  }
+}
+
+
+</script>
+
+<template>
+  <h1>
+    Neurtuta
+  </h1>
+  <h4>
+    you're personal mesures
+  </h4>
+
+  <main>
+    <div class="mycalories-calculator">
+
+      <label for="weight">Weight in kg</label>
+      <input v-model="store.myData.weight" type="text" id="weight">
+      <br>
+      <label for="height">Height in cm</label>
+      <input v-model="store.myData.height" type="text" id="height">
+      <br>
+      <label for="age">Age</label>
+      <input id="insert age" v-model="store.myData.age" type="text">
+      <br>
+      <select v-model="store.myData.gender" id="gender">
+        <option disabled value="">Select gender</option>
+        <option value="male">Male</option>
+        <option value="female">Female</option>
+      </select>
+      <br>
+      <div>Select number of workout in a week</div>
+      <input v-model="store.myData.numberWorkouts" type="radio" id="one" value="1.1">
+      <label for="one">One</label>
+
+      <input v-model="store.myData.numberWorkouts" type="radio" id="two" value="1.2">
+      <label for="two">Two</label>
+
+      <input v-model="store.myData.numberWorkouts" type="radio" id="three" value="1.3">
+      <label for="three">Three</label>
+      <br>
+      <button @click="(newCaloriesCalculation)">
+        Calculate
+      </button>
+
+    </div>
+    <div>
+      Your BMI is {{ store.myData.caloriesBMI }}
+    </div>
+    <div>
+      Your calories with workout {{ store.myData.calories }}
+    </div>
+
+    <button @click="(debugbutton)">
+      DEBUG
+    </button>
+  </main>
+</template>
+
+<style lang="scss">
+@use './components/general.scss' as*;
+
+h1 {
+  text-align: center;
+}
+</style>
---
+++ vite-neurtuta/src/store.js
@@ -0,0 +1,17 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    myData: {
+        gender: '',
+        height: '',
+        age: '',
+        weight: '',
+        numberWorkouts: 0,
+        caloriesBMI: 0,
+        calories: 0,
+    }
+})
+
+// 10 * weight (kg) + 6.25 * height(cm) - 5 * age(y) + 5 for (man)
+
+// 10 * weight(kg) + 6.25 * height(cm) - 5 * age(y) - 161 for ​(woman)
